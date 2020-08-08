9cdb7ef8e52a41cb4517ea75c52f46225d325def
Bhanu <bpst07@gmail.com>
2020-08-08 17:30:43 +0530

    Final Code Commit

---
+++ app/src/main/java/com/arrow/fragment/Constants.kt
@@ -0,0 +1,6 @@
+package com.arrow.fragment
+
+object Constants {
+    const val PARAM_1 = "Message to Factory Fragment"
+    const val PARAM_2 = "Wow, its awesome"
+}
--- app/src/main/java/com/arrow/fragment/MainActivity.kt
+++ app/src/main/java/com/arrow/fragment/MainActivity.kt
@@ -2,10 +2,28 @@ package com.arrow.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arrow.fragment.Constants.PARAM_1
+import com.arrow.fragment.Constants.PARAM_2
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var blankFragment: BlankFragment
+    private val blankFragmentTag: String = "Blank Fragment"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // instantiate blank fragment
+        blankFragment = BlankFragment.newInstance(PARAM_1, PARAM_2)
+
+        // add "BlankFragment" to container (Main Activity)
+        // provide tag name to handle configuration changes
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, blankFragment, blankFragmentTag)
+                    .commit()
+        }
     }
 }
